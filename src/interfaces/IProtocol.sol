01e8bf9ce26ae74975e6cfcc1286a3d30146e299
ichristwin <ichristwin@gmail.com>
2024-07-26 01:50:00 +0100

    minimized protocol interface

--- src/interfaces/IProtocol.sol
+++ src/interfaces/IProtocol.sol
@@ -1,30 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-error BadClaim();
-error BadModule();
-error InputIsZero();
-error ZeroAddress();
-error Unauthorized();
-
 interface IProtocol {
-    event Revenue(
-        uint256 indexed tokenId, uint256 indexed amount, uint256 indexed tariff, address from, uint256 timestamp
-    );
-
-    event Claim(address indexed to, uint256 indexed amount, uint256 indexed timestamp);
-
-    function _curateModule(address CLMAddress, bool state) external;
-
-    function _setFeeAddress(address otherAddress) external;
-
-    function _setContractId(uint256 tokenId, string memory contractId) external;
-
-    function _setTariff(uint256 tokenId, uint256 tariff) external;
-
     function pay(uint256 tokenId) external payable;
-
-    function claim(address CLMAddress, bytes calldata data) external;
-
-    function tariffOf(uint256 tokenId) external view returns (uint256);
 }
